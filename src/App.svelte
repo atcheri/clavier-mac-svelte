b19f4c1e9f031fbf8694f9bbfe83f3b3ec6b78cb
Atcheri Atch <atcheri@gmail.com>
2023-01-24 03:05:42 +0100

    hajime no commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main
+  class="flex flex-col justify-center items-center h-screen w-screen bg-gray-100"
+>
+  <h1>Virtual Mac-book Pro keyboard</h1>
+  <h2>Made with Svelte and Tailwindcss in TS</h2>
+</main>
+
+<style>
+</style>
